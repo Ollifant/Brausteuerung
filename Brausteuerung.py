bdcbedaff7f65b4a82b432cc0c786369747541da
Ollifant <oliver@bohl.info>
2021-02-16 20:44:11 +0100

    Tagessicherung

--- Brausteuerung.py
+++ Brausteuerung.py
@@ -28,9 +28,9 @@ class Switch:
         GPIO.setup(self.Pin, GPIO.OUT)
         # Steckdose schalten HIGH = AUS!!!
         GPIO.output(self.Pin, GPIO.HIGH)
+        # Status der Steckdose ist ausgeschaltet
         self.State = False
-        self.txt = "Init {} at Pin {}"
-        print( self.txt.format(self.Name, self.Pin))
+        print("Init {} at Pin {}".format(self.Name, self.Pin))
 
     def On(self):
         # Wenn Steckdose aus, dann einschalten
@@ -38,8 +38,7 @@ class Switch:
             # Steckdose schalten LOW = AN!!!
             GPIO.output(self.Pin, GPIO.LOW)
             self.State = True
-            self.txt = "{} {} On"
-            print (self.txt.format(Timestamp(), self.Name))
+            print("{} {} On".format(Timestamp(), self.Name))
     
     def Off(self):
         # Wenn Steckdose an, dann ausschalten
@@ -47,121 +46,176 @@ class Switch:
             # Steckdose schalten High = AUS!!!
             GPIO.output(self.Pin, GPIO.HIGH)
             self.State = False
-            self.txt = "{} {} Off"
-            print (self.txt.format(Timestamp(), self.Name))
+            print("{} {} Off".format(Timestamp(), self.Name))
+            
+class Brew:
+    def __init__(self, heizGPIO, ruehrGPIO):
+        #Leere Listen erzeugen
+        self.TempList = [float]
+        self.SollList = [float]
+        # Rote Steckdose - Heizung
+        self.RedSwitch = Switch(heizGPIO, "RedSwitch")
+        # Blaue Steckdos - Rührer
+        self.BlueSwitch = Switch(ruehrGPIO, "BlueSwitch")
+        # Rührwerk einschalten
+        self.BlueSwitch.On()
+
+    def ReadTemperature(self, SollTemp):  
+        # Temperatur auslesen
+        self.SensorTemp = float(Sensor.temperature)
+        # Temperatur(en) der Liste(n) hinzufügen
+        self.TempList.append(self.SensorTemp)
+        self.SollList.append(SollTemp)
+        # Return Temperature
+        return self.SensorTemp
+        
+    def HoldTemperature(self, Temperature, Duration, Hysteresis):
+        print ("{} Solltemperatur {} Dauer {} Minute(n)".format(Timestamp(),Temperature, Duration))
+        # Aufheizen, bis Temperatur erreocht
+        print ("{} Aufheizen auf {} Grad".format(Timestamp(), Temperature))
+        self.temp = self.ReadTemperature(Temperature)
+        while self.temp < Temperature:
+            # Schaltet Heizung ein, wenn vorher aus
+            self.RedSwitch.On()
+            print('{} Temperature: {:0.3f} C'.format(Timestamp(), self.temp))
+            # Dealy 1 second
+            time.sleep(1.0)
+            # Temperatur neu lesen
+            self.temp = self.ReadTemperature(Temperature)
+            
+        # Hält die Temperatur, bis Zeit abgelaufen ist
+        self.StartTime = time.time()
+        self.EndTime = self.StartTime + (Duration * 60)
+         
+        print("{} Temperatur {} Grad für {} Minute(n) halten".format(Timestamp(), Temperature, Duration))
+        while self.EndTime > time.time():
+            self.temp = self.ReadTemperature(Temperature)
+            if self.temp < (Temperature - Hysteresis):
+                # Temperatur ist kleiner Solltemperatur minus Hysterese
+                # Heizung einschalten
+                self.RedSwitch.On()
+            else:
+                # Heizung ausschalten
+                self.RedSwitch.Off()
+            print('{} Temperature: {:0.3f} C'.format(Timestamp(), self.temp))
+            # Delay 1 Second
+            time.sleep(1.0)
+        print("{} Time over".format(Timestamp()))
+        
+    def makeJodprobe(self, jTemperature, jDuration, jHysteresis):
+        print("Jodprobe jetzt")
+        self.userInput = str(input("Jodprobe erfolgreich? j/n: "))
+        while ((self.userInput != "j") and (self.userInput != "n")):
+            # Eingabe wiederholen
+            self.userInput = str(input("Jodprobe erfolgreich? j/n: "))
+        if self.userInput == "n":
+            # Zeit auf der Temperaturstufe wird verlängert
+            self.HoldTemperature(jTemperature, jDuration, jHysteresis)
+            # Jodprobe war nicht erfolgreich und muss ggf. wiederholt werden
+            return False
+        else:
+            # Jodprobe war erfolgreich
+            return True
+        
+    def mashing(self):
+        #Hysterese ermitteln
+        try:
+            self.Hysteresis = Mash["Hyst"]
+        except:
+            self.Hysteresis = 0.2
+        print("{} Grad Hysterese".format(self.Hysteresis))
+
+        # zusätzliche Zeit für Jodprobe ermitteln
+        try:
+            self.TimeAdd = Mash["TimeAdd"]
+        except:
+            self.TimeAdd = 10
+        print("{} min extra wenn Jodprobe negativ".format(self.TimeAdd))
+
+        # Temperaturrasten ansteuern
+        self.x = 1
+        while (True):
+            try:
+                self.tempX = "Temp" + str(self.x)
+                self.durX = "Duration" + str(self.x)
+                # Prüfen, ob es einen Eintrag (Temperatur und Dauer) gibt
+                if (Mash[self.tempX] > 0) and (Mash[self.durX] > 0):
+                    self.HoldTemperature(Mash[self.tempX], Mash[self.durX], self.Hysteresis)
+                    # Jodprobe erforderlich?
+                    try:
+                        self.jodProbe = "Jodprobe" + str(self.x)
+                        if Mash[self.jodProbe] == True:
+                            # Jodprobe so lange durchführen, bis erfolgreich
+                            print("Jodprobe machen temp: {} TimeAdd: {} Hysteresis: {}".format(Mash[self.tempX], self.TimeAdd, self.Hysteresis))
+                            while (self.makeJodprobe(Mash[self.tempX], self.TimeAdd, self.Hysteresis) != True):
+                                pass
+                    except:
+                        pass
+                    #Nächster Eintrag
+                    self.x = self.x+1
+            except:
+                self.x = self.x-1
+                print("{} Brauvorgang abgeschlossen {} Raste(n)".format(Timestamp(),self.x))
+                # Alles ausschalten
+                self.RedSwitch.Off()
+                self.BlueSwitch.Off()
+                
+                # Ergebnis plotten
+                #self.PrintGraph()
+                break
+       
+        
+    def PrintGraph(self):
+        matplotlib.use('tkagg')
+        #matplotlib.use('Agg')
+
+        # Anzahl der Messpunkte anzeigen
+        print("Anzahl Meßpunkte: {}".format(len(self.TempList)))
+
+        # Style benutzen (überschreibt einige eigene Definitionen)
+        #plt.style.use('fivethirtyeight')
+        plt.style.use('seaborn-darkgrid')
+
+        # Zeichne Ist-Temperatur
+        self.yPoints1 = np.array(self.TempList)
+        plt.plot(self.yPoints1, marker = '.', label=r'Ist Temp.')
+        # Zeichne Soll-Temperatur
+        self.yPoints2 = np.array(self.SollList)
+        plt.plot(self.yPoints2, marker = '.', label=r'Soll Temp.')
+
+        # Array mit Anzahl Meßpunkte erzeugen
+        self.xPoints = np.arange(0.,len(self.TempList),1)
+        # Fläche oberhalb blau einfärben und mit Label beschriften
+        plt.fill_between(self.xPoints, self.yPoints1, self.yPoints2, where=(self.yPoints1 > self.yPoints2), interpolate=True, color='blue', alpha=.1, label='Kühlphase(n)')
+        # Fläche oberhalb rot einfärben und mit Label beschriften
+        plt.fill_between(self.xPoints, self.yPoints1, self.yPoints2, where=(self.yPoints1 <= self.yPoints2), interpolate=True, color='red', alpha=.1, label='Heizphase(n)')
+
+        # Legende einblenden:
+        plt.legend(loc='upper left', frameon=False)
+
+        # y-Achse festlegen
+        plt.axis( [0, len(self.TempList), (min(self.TempList)-5), (max(self.TempList)+5)])
+        
+        self.font1 = {'color':'blue','size':18}
+        self.font2 = {'size':15}
+        plt.title("Temperaturverlauf", fontdict = self.font1)
+        plt.xlabel("Zeit [Sek]", fontdict = self.font2)
+        plt.ylabel("Temperatur [Grad C]", fontdict = self.font2)
+        plt.grid(axis = 'y')
+        # Nur mit Agg
+        #plt.savefig('TempVerlauf.png', bbox_inches='tight')
+
+        # Nur mit Tkgg auf Headless Device
+        plt.show()
+        #plt.savefig(sys.stdout.buffer)
+        #sys.stdout.flush()
+
         
 # Initialize MAX31865 board
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 # Chip select of the MAX31865 board GPIO 23
 cs = digitalio.DigitalInOut(board.D23)  
 Sensor = adafruit_max31865.MAX31865(spi, cs)
-
-#Leere Listen erzeugen
-TempList = []
-SollList = []
-
-def ReadTemperature(SollTemp):  
-    # Temperatur auslesen
-    SensorTemp = float(Sensor.temperature)
-    # Temperatur(en) der Liste(n) hinzufügen
-    TempList.append(SensorTemp)
-    SollList.append(SollTemp)
-    # Return Temperature
-    return SensorTemp
-
-def HoldTemperature(Temperature, Duration, Hysteresis):
-    
-    print ("{} Solltemperatur {} Dauer {} Minute(n)".format(Timestamp(),Temperature, Duration))
-    
-    # Aufheizen, bis Temperatur erreocht
-    print ("{} Aufheizen auf {} Grad".format(Timestamp(), Temperature))
-    temp = ReadTemperature(Temperature)
-    while temp < Temperature:
-        # Schaltet Heizung ein, wenn vorher aus
-        RedSwitch.On()
-        print('{} Temperature: {:0.3f} C'.format(Timestamp(), temp))
-        # Dealy 1 second
-        time.sleep(1.0)
-        # Temperatur neu lesen
-        temp = ReadTemperature(Temperature)
-        
-    # Hält die Temperatur, bis Zeit abgelaufen ist
-    StartTime = time.time()
-    EndTime = StartTime + (Duration * 60)
-     
-    print ("{} Temperatur {} Grad für {} Minute(n) halten".format(Timestamp(), Temperature, Duration))
-    while EndTime > time.time():
-        temp = ReadTemperature(Temperature)
-        if temp < (Temperature - Hysteresis):
-            # Temperatur ist kleiner Solltemperatur minus Hysterese
-            # Heizung einschalten
-            RedSwitch.On()
-        else:
-            # Heizung ausschalten
-            RedSwitch.Off()
-        print('{} Temperature: {:0.3f} C'.format(Timestamp(), temp))
-        # Delay 1 Second
-        time.sleep(1.0)
-    print("{} Time over".format(Timestamp()))
-    
-def makeJodprobe(jTemperature, jDuration, jHysteresis):
-    userInput = str(input("Jodprobe erfolgreich? j/n: "))
-    while ((userInput != "j") and (userInput != "n")):
-        # Eingabe wiederholen
-        userInput = str(input("Jodprobe erfolgreich? j/n: "))
-    if userInput == "n":
-        # Zeit auf der Temperaturstufe wird verlängert
-        HoldTemperature(jTemperature, jDuration, jHysteresis)
-        # Jodprobe war nicht erfolgreich und muss ggf. wiederholt werden
-        return False
-    else:
-        # Jodprobe war erfolgreich
-        return True
-    
-def PrintGraph():
-    matplotlib.use('tkagg')
-    #matplotlib.use('Agg')
-
-    # Anzahl der Messpunkte anzeigen
-    print("Anzahl Meßpunkte: {}".format(len(TempList)))
-
-    # Style benutzen (überschreibt einige eigene Definitionen)
-    #plt.style.use('fivethirtyeight')
-    plt.style.use('seaborn-darkgrid')
-
-    # Zeichne Ist-Temperatur
-    yPoints1 = np.array(TempList)
-    plt.plot(yPoints1, marker = '.', label=r'Ist Temp.')
-    # Zeichne Soll-Temperatur
-    yPoints2 = np.array(SollList)
-    plt.plot(yPoints2, marker = '.', label=r'Soll Temp.')
-
-    # Array mit Anzahl Meßpunkte erzeugen
-    xPoints = np.arange(0.,len(TempList),1)
-    # Fläche oberhalb blau einfärben und mit Label beschriften
-    plt.fill_between(xPoints, yPoints1, yPoints2, where=(yPoints1 > yPoints2), interpolate=True, color='blue', alpha=.1, label='Kühlphase(n)')
-    # Fläche oberhalb rot einfärben und mit Label beschriften
-    plt.fill_between(xPoints, yPoints1, yPoints2, where=(yPoints1 <= yPoints2), interpolate=True, color='red', alpha=.1, label='Heizphase(n)')
-
-    # Legende einblenden:
-    plt.legend(loc='upper left', frameon=False)
-
-    # y-Achse festlegen
-    plt.axis( [0, len(TempList), (min(TempList)-5), (max(TempList)+5)])
-    
-    font1 = {'color':'blue','size':18}
-    font2 = {'size':15}
-    plt.title("Temperaturverlauf", fontdict = font1)
-    plt.xlabel("Zeit [Sek]", fontdict = font2)
-    plt.ylabel("Temperatur [Grad C]", fontdict = font2)
-    plt.grid(axis = 'y')
-    # Nur mit Agg
-    #plt.savefig('TempVerlauf.png', bbox_inches='tight')
-
-    # Nur mit Tkgg auf Headless Device
-    plt.show()
-    #plt.savefig(sys.stdout.buffer)
-    #sys.stdout.flush()
 
     
 Mash = {
@@ -181,60 +235,14 @@ Mash = {
     "Jodprobe2" : True
     }
 
-# Rote Steckdose - Heizung
-RedSwitch = Switch(17, "RedSwitch")
-
-# Blaue Steckdos - Rührer
-BlueSwitch = Switch(18, "BlueSwitch")
+# Rote Steckdose GPIO
+redGPIO = 17
+# Blaue Steckdose GPIO
+blueGPIO = 18
 
 # Main
-# Rührwerk einschalten
-BlueSwitch.On()
-
-#Hysterese ermitteln
-try:
-    Hysteresis = Mash["Hyst"]
-except:
-    Hysteresis = 0.2
-print ("{} Grad Hysterese".format(Hysteresis))
-
-# zusätzliche Zeit für Jodprobe ermitteln
-try:
-    TimeAdd = Mash["TimeAdd"]
-except:
-    TimeAdd = 10
-print ("{} min extra wenn Jodprobe negativ".format(TimeAdd))
-
-# Temperaturrasten ansteuern
-x = 1
-while (True):
-    try:
-        temp = "Temp" + str(x)
-        dur = "Duration" + str(x)
-        # Prüfen, ob es einen Eintrag (Temperatur und Dauer) gibt
-        if (Mash[temp] > 0) and (Mash[dur] > 0):
-            HoldTemperature(Mash[temp], Mash[dur], Hysteresis)
-            # Jodprobe erforderlich?
-            try:
-                jodProbe = "Jodprobe" + str(x)
-                if Mash[jodProbe] == True:
-                    # Jodprobe so lange durchführen, bis erfolgreich
-                    while makeJodprobe(Mash[temp], TimeAdd, Hysteresis) != True:
-                        pass
-            except:
-                pass
-            #Nächster Eintrag
-            x = x+1
-    except:
-        x = x-1
-        text = "{} Brauvorgang abgeschlossen {} Raste(n)"
-        print(text.format(Timestamp(),x))
-        # Alles ausschalten
-        RedSwitch.Off()
-        BlueSwitch.Off()
-        
-        # Ergebnis plotten
-        PrintGraph()
-        break
-    
+# Brauprogramm initialisieren
+brew = Brew(redGPIO, blueGPIO)
+# Brauprogramm ablaufen lassen
+brew.mashing()
     
