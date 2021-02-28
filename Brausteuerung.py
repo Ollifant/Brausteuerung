eb5ae318ea1540965c39ad71961015dd64ad3a59
Ollifant <oliver@bohl.info>
2021-02-28 16:54:54 +0100

    Rotary Encoder implementiert

--- Brausteuerung.py
+++ Brausteuerung.py
@@ -18,6 +18,11 @@ from luma.oled.device import sh1106, ssd1306
 from PIL import ImageFont, ImageDraw, Image
 # ---------------------------------------
 
+# --------------Encoder Lib -------------
+# pip3 install encoder (um die Lib zu installieren
+import Encoder
+# ---------------------------------------
+
 #OLED Display auf I2C Bus initialisieren
 serial = i2c(port=1, address=0x3C)
 device = sh1106(serial)
@@ -40,6 +45,12 @@ redGPIO = 17
 blueGPIO = 18
 # Beeper GPIO
 beepGPIO = 24
+# Encoder GPIO 1
+encoderDrehGPIO_1 = 14
+# Encoder GPIO 2
+encoderDrehGPIO_2 = 15
+# Encoder Push GPIO
+encoderPushGPIO = 25
 
 def Timestamp():
     # Funktion ermittelt die aktuelle Zeit in hh:mm:ss
@@ -93,7 +104,7 @@ class Switch:
             print("{} {} Off".format(Timestamp(), self.Name))
             
 class Brew:
-    def __init__(self, heizGPIO, ruehrGPIO, beeperGPIO):
+    def __init__(self, heizGPIO, ruehrGPIO, beeperGPIO, dreh1GPIO, dreh2GPIO, pushGPIO):
         #Leere Listen erzeugen
         self.TempList = []
         self.SollList = []
@@ -111,6 +122,11 @@ class Brew:
         
         # Beeper
         self.beeper = Beeper(beeperGPIO)
+        
+        # Encoder
+        self.dreh = Encoder.Encoder(dreh1GPIO, dreh2GPIO)
+        GPIO.setup(pushGPIO, GPIO.IN)
+        GPIO.add_event_detect(pushGPIO, GPIO.FALLING, callback=self.pushButton, bouncetime=30)
         
         #Datenbank initialisieren
         self.initDB()
@@ -239,20 +255,46 @@ class Brew:
             else:
                 # Heizung ausschalten
                 self.RedSwitch.Off()
-            #print('{} Temperature: {:0.3f} C'.format(Timestamp(), self.temp))
+            #print('{} Temperature: {:0.3f} C'.format(Timestamp(), self.temp)
             # Delay 1 Second
             time.sleep(1.0)
-        print("{} Time over".format(Timestamp()))
+        #print("{} Time over".format(Timestamp()))
         
     def makeJodprobe(self, jTemperature, jDuration, jHysteresis):
         # User muss Jodprobe machen
         # Aufmerksamkeitston
         self.beeper.makeBeep(0.5, 0)
-        with canvas(device) as draw:
-            #draw.rectangle(device.bounding_box, outline = "white", fill = "black")
-            draw.text((15, 10), "Jodprobe !!!", font = oled_font, fill = "white")
+        
+        #Stellung des Encoders lesen
+        self.DrehZahl = self.dreh.read()
+        self.AlteZahl = 0
+        self.buttonState = False
+        self.anzeige = "Ergebnis: Nein"
+        self.result = False
+        while self.buttonState != True:
+            with canvas(device) as draw:
+                #draw.rectangle(device.bounding_box, outline = "white", fill = "black")
+                draw.text((15, 0), "Jodprobe", font = oled_font, fill = "white")
+                draw.text((15, 20), "erfolgreich?", font = oled_font, fill = "white")
+                if ((self.DrehZahl > (self.AlteZahl + 6)) or (self.DrehZahl < (self.AlteZahl - 6))):
+                    # Stellung des Encoders hat sich signifikant geändert
+                    # Anzeige ändern
+                    if self.anzeige == 'Ergebnis: Ja':
+                        self.anzeige = "Ergebnis: Nein"
+                        self.result = False
+                    else: 
+                        self.anzeige = "Ergebnis: Ja"
+                        self.result = True
+                    # Neu Zahl speichern
+                    self.AlteZahl = self.DrehZahl
+            
+                draw.text((15, 40), self.anzeige, font = oled_font, fill = "white")
+                time.sleep(0.25)
+                #Stellung des Encoders erneut lesen
+                self.DrehZahl = self.dreh.read()
+        
         # Wenn Jodprobe nicht erfolgreich, dann Raste verlängern
-        if self.userInputJN("Jodprobe erfolgreich?") == False:
+        if self.result == False:
             # Zeit auf der Temperaturstufe wird verlängert
             self.HoldTemperature(jTemperature, jDuration, jHysteresis)
             # Jodprobe war nicht erfolgreich und muss ggf. wiederholt werden
@@ -336,16 +378,26 @@ class Brew:
         #plt.savefig(sys.stdout.buffer)
         #sys.stdout.flush()
         
+    def pushButton(self, channel):
+        # Methode wird aufgerufen, wenn Button des Encoders gedrückt wurde
+        self.buttonState = True
+        
+        
     def get_hms(self, delta):
         # Return hours, minutes and seconds for a given `timedelta`-object.
         h, m = divmod(delta, 3600)
         m, s = divmod(m, 60)
-        return ("{:0>2d}:{:0>2d}:{:0>2d}".format(int(h), int(m), int(s)))
-    
+        if h > 0:
+            # Zeit größer einer Stunde = Stunde mit ausgeben
+            return ("{:0>2d}:{:0>2d}:{:0>2d}".format(int(h), int(m), int(s)))
+        else:
+            # Zeit kleiner einer Stunde = ohne Stunde ausgeben
+            return ("{:0>2d}:{:0>2d}".format(int(m), int(s)))
+ 
 
 # Main
 # Brauprogramm initialisieren
-brew = Brew(redGPIO, blueGPIO, beepGPIO)
+brew = Brew(redGPIO, blueGPIO, beepGPIO, encoderDrehGPIO_1, encoderDrehGPIO_2, encoderPushGPIO)
 # Brauprogramm ablaufen lassen
 if brew.importConfig() == True:
     brew.mashing()
